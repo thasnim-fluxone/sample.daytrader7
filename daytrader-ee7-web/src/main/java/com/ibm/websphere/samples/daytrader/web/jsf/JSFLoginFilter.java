7e1e5bd50a3c0bddd9a83c33f515935cdd026bfb
thasnim-easejava <thasnim.hm@gmail.com>
2025-02-02 19:14:28 +0530

    Update JSFLoginFilter.java

--- daytrader-ee7-web/src/main/java/com/ibm/websphere/samples/daytrader/web/jsf/JSFLoginFilter.java
+++ daytrader-ee7-web/src/main/java/com/ibm/websphere/samples/daytrader/web/jsf/JSFLoginFilter.java
@@ -29,7 +29,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-// @WebFilter(filterName = "JSFLoginFilter", urlPatterns = "*.faces")
+@WebFilter(filterName = "JSFLoginFilter", urlPatterns = "*.faces")
 public class JSFLoginFilter implements Filter {
 
     public JSFLoginFilter() {
